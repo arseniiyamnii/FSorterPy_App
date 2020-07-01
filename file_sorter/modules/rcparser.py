3150c724b342464bfb5163593894f64639da339b
Arsenii Yamnii <yamnii@ya.ru>
2020-07-02 00:06:50 +0500

    send path to organizerc

--- file_sorter/modules/rcparser.py
+++ file_sorter/modules/rcparser.py
@@ -1,4 +1,6 @@
 class rcparser():
+    def __init__(self, path_to_organizerc):
+        self.path_to_organizerc=path_to_organizerc
     def check_rc(dirrectory):
         '''this function must return existense rc file in some dirrectory
             dirrectory starts from /, not from home.'''
@@ -9,3 +11,5 @@ class rcparser():
         except IOError: 
             exist = False
         return exist
+    def get_path_to_organizerc(self):
+        return self.path_to_organizerc
--- tests/test_rcparser.py
+++ tests/test_rcparser.py
@@ -1,11 +1,25 @@
 import pytest
 
-
 from ..file_sorter.modules.rcparser import rcparser
+
 
 def test_check_rc_exist():
     rcparser.check_rc
 def test_checkrc():
-    assert rcparser.check_rc("./tests/testFiles/.organizerc") == True
+    assert rcparser.check_rc("./file_sorter/res/.organizerc") == True
     assert rcparser.check_rc("./tests/testFiles/.non_existing_file") == False
 
+def test_init():
+    rc=rcparser("./file_sorter/res/.organizerc")
+    assert rc is not None
+
+@pytest.fixture
+def rcparser_object():
+    rc=rcparser("./file_sorter/res/.organizerc")
+    return rc
+
+def test_get_path_to_organizerc_exist(rcparser_object):
+    rcparser_object.get_path_to_organizerc()
+
+def test_get_path_to_organizerc(rcparser_object):
+    assert rcparser_object.get_path_to_organizerc() == "./file_sorter/res/.organizerc"
